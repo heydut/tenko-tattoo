c75cd3e807138df4f4b56ee398afe8fbe32861ef
Mayranne <mayranne-@hotmail.com>
2023-10-26 14:57:43 +0100

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Btn from "./UI/Btn";
-import NavBanner from "./NavBanner";
 import { Navbar, NavbarContent } from "@nextui-org/react";
 
 // ✨ //////////////////////////////
