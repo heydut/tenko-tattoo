af0a1f3665e9dcc766bc7b082cd275d909df78f9
Mayranne <mayranne-@hotmail.com>
2023-10-26 15:02:42 +0100

    fixing instagram fetch

--- src/hooks/getInstagramPosts.jsx
+++ src/hooks/getInstagramPosts.jsx
@@ -1,15 +1,24 @@
 const getInstagramPosts = async () => {
-  const res = await fetch(
-    `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,timestamp,permalink&access_token=${process.env.INSTAGRAM_TESTUSER_TOKEN}`,
-    {
-      next: {
-        revalidate: 21600,
-      },
-    }
-  );
+  try {
+    const response = await fetch(
+      `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,timestamp,permalink&access_token=${process.env.INSTAGRAM_TESTUSER_TOKEN}`,
+      {
+        next: {
+          revalidate: 21600,
+        },
+      }
+    );
 
-  const data = await res.json();
-  return data;
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching Instagram data:", error);
+    throw error; // Re-throw the error for further handling, if needed
+  }
 };
 
 export default getInstagramPosts;
